ac4146b57ac858fb6e98b43c94e85ae5bc22d906
mxz-dev <zabetzadehmohmmad@gmail.com>
2025-06-09 17:10:39 +0330

    change in perimission.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,11 +1,15 @@
 from rest_framework import permissions
 
 class IsProfileOwenerOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
     def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser:
-            return True 
         if request.method == permissions.SAFE_METHODS:
             return True
+        if request.user.is_superuser:
+            return True 
         if request.user == obj.user:
             return True
         return False
